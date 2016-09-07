6789b3398f65aa3a4156c0d77c8f05efc68943db
crucis <bandeira85@gmail.com>
2016-09-07 02:56:07 -0300

    Changed evaluation algorithm, now using FIT directly to get val_acc and acc

--- Tarefa1/FCduascamadas.py
+++ Tarefa1/FCduascamadas.py
@@ -22,7 +22,7 @@ numpy.random.seed(SEED)
 TRAININGPOINTSPERCENTAGE = 1
 BATCHSIZE = 5
 EPOCH = 200
-VALIDATION% = 0.10
+VALIDATIONPERC = 0.15
 
 ########################
 
@@ -43,38 +43,30 @@ dummy_y = np_utils.to_categorical(encoded_Y)
 # Vector with hidden_nodes on second layer
 hidden_nodes = [10,100,1000,10000]
 print("----------------------------------")
-print("Using Batch =",BATCHSIZE ,"Epoch = ",EPOCH, "Seed = ",SEED)
+print("Using Batch =",BATCHSIZE ,"Epoch = ",EPOCH, "Validation_split = ",VALIDATIONPERC)
 
 # Create len(hidden_nodes) neural networks
 for i in hidden_nodes:
-# create model
 	# time measure
 	start_time = time.time()
 	# Model
 	print("----------------------------------")
 	print("Creating model with hidden_nodes =",i)
+	# create model
 	def baseline_model():
-		# create model
 		model = Sequential()
-		# Camada com i neurônios
+		# layer with i nodes
 		model.add(Dense(i, input_dim=4, init='normal', activation='relu'))
-		# Camada de saída, 3 categorias
+		# output layer
 		model.add(Dense(3, init='normal', activation='softmax'))
 		# Compile model
 		model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 		return model
-
-
-# evaluate the model
-	#KerasClassifier class will pass on to the fit() function internally all arguments used to train the neural network. # of epochs, batch size and debugging (0 = off).
-	estimator = KerasClassifier(build_fn=baseline_model, nb_epoch=EPOCH, batch_size=BATCHSIZE, verbose=0)
-
-	#cross-validation uses 100% of the data. At each iteration, some p% of data is trained on, and (100-p)% is tested on, and the average score is returned.
-	kfold = KFold(n=len(X), n_folds=10, shuffle=True, random_state=SEED)
-	results = cross_val_score(estimator,X,dummy_y,cv=kfold)
-	#The results are summarized as both the mean and standard deviation of the model accuracy on the dataset. This is a reasonable estimation of the performance of the model on unseen data.
-	print("Baseline:%.2f%%(%.2f%%)" %(results.mean()*100,results.std()*100))
+	# Fit model	
+	history = baseline_model().fit(X,dummy_y,validation_split=VALIDATIONPERC,nb_epoch=EPOCH,batch_size=BATCHSIZE,verbose=0)
+	print("val_acc = %.4f"%history.history["val_acc"][EPOCH-1]," acc = %.4f"%history.history["acc"][EPOCH-1])		
 	# Show time elapsed
-	print("elapsed time = %.2f s"%(time.time()-start_time))
+	print("Elapsed time = %.2f s"%(time.time()-start_time))
 
 
+# eof
