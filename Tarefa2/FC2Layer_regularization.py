2a197dbebfdc2a1c47d4edf3ded30c8483f91c28
crucis <bandeira85@gmail.com>
2016-09-10 23:59:17 -0300

    added option to train with more hidden_nodes. Creates a folder for each

--- Tarefa2/FC2Layer_regularization.py
+++ Tarefa2/FC2Layer_regularization.py
@@ -1,4 +1,5 @@
 #Aprender regularizacao: estudar regularizacao (no livro ou na internet), principalmente regularizacao L1, L2 e tecnica Dropout. Com 2 camadas e 1000 neuronios ocultos, criar redes com L1 = 0.001, L2 = 0.001 e dropout = 0.2 (total de 3 redes, cada tecnica individual em uma).Plotar trainAcc e validAcc para cada, de acordo com a epoch. 
+
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
@@ -19,7 +20,7 @@ import time
 ########################
 
 # Graphs options
-SHOW_GRAPHS = 1
+SHOW_GRAPHS = 0
 SAVE_GRAPHS = 1
 SHOW_LOSS = 1
 SHOW_ACC = 1
@@ -38,11 +39,24 @@ VALIDATIONPERC = 0.15
 l1Reg = 0.001
 l2Reg = 0.001
 dropout = 0.2
-hidden_nodes = 1000 # Vector with hidden_nodes on second layer
+hidden_nodes = [10,100,1000,10000] # Vector with hidden_nodes[h] on second layer
 
 ########################
 #FUNCTIONS
 ########################
+
+def mkdir_p(mypath):
+    #Creates a directory. equivalent to using mkdir -p on the command line'''
+
+    from errno import EEXIST
+    from os import makedirs,path
+
+    try:
+        makedirs(mypath)
+    except OSError as exc: # Python >2.5
+        if exc.errno == EEXIST and path.isdir(mypath):
+            pass
+        else: raise
 
 # defining function to display and/or save graphs
 def plotGraph (filename, nodes, vecTrain, vecTest, nameVec):
@@ -57,7 +71,9 @@ def plotGraph (filename, nodes, vecTrain, vecTest, nameVec):
 	plt.xlabel('epoch')
 	plt.legend(['train', 'test'], loc='lower right')
 	if SAVE_GRAPHS == 1:
-		figurestr = "results/"+filename+"_"+nameVec+".png"
+		output_dir="results/"+str(nodes)+"nodes"
+		mkdir_p(output_dir) # Verifies if directory exists, and creates it if necessary
+		figurestr = output_dir+"/"+filename+"_"+nameVec+".png"
 		plt.savefig(figurestr)
 	if SHOW_GRAPHS == 1:
 		plt.show()
@@ -87,50 +103,51 @@ print("Using Batch =",BATCHSIZE ,"Epoch = ",EPOCH, "Validation_split = ",VALIDAT
 
 
 # Create 3 neural networks, each with different regularization methods
-for i in range (0,3):
-	# time measure
-	start_time = time.time()
+for h in range(0, len(hidden_nodes)):
+	for i in range (0,3):
+		# time measure
+		start_time = time.time()
 
-	########################
-	# Model
-	print("----------------------------------")
-	print("Creating model with hidden_nodes =",hidden_nodes)
-	# create model
+		########################
+		# Model
+		print("----------------------------------")
+		print("Creating model with hidden_nodes =",hidden_nodes[h])
+		# create model
 
-	model = Sequential()
-	# layer with hidden_nodes nodes
-	if i == 0:
-		model.add(Dense(hidden_nodes, input_dim=4, init='normal', activation='relu', W_regularizer = l1(l1Reg)))
-		regStr = "L1_Regularization="+str(l1Reg)
-	elif i == 1:
-		model.add(Dense(hidden_nodes, input_dim=4, init='normal', activation='relu', W_regularizer = l2(l2Reg)))
-		regStr = "L2_Regularization="+str(l2Reg)
-	else:
-		model.add(Dense(hidden_nodes, input_dim=4, init='normal', activation='relu'))
-		model.add(Dropout(dropout))
-		regStr = "Dropout="+str(dropout)
-	print("Added",regStr)
-	# output layer
-	model.add(Dense(3, init='normal', activation='softmax'))
-	# Compile model
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+		model = Sequential()
+		# creates layer with hidden_nodes nodes and chooses which regularization it will use
+		if i == 0:
+			model.add(Dense(hidden_nodes[h], input_dim=4, init='normal', activation='relu', W_regularizer = l1(l1Reg)))
+			regStr = "L1_Regularization="+str(l1Reg)
+		elif i == 1:
+			model.add(Dense(hidden_nodes[h], input_dim=4, init='normal', activation='relu', W_regularizer = l2(l2Reg)))
+			regStr = "L2_Regularization="+str(l2Reg)
+		else:
+			model.add(Dense(hidden_nodes[h], input_dim=4, init='normal', activation='relu'))
+			model.add(Dropout(dropout))
+			regStr = "Dropout="+str(dropout)
+		print("Added",regStr)
+		# output layer
+		model.add(Dense(3, init='normal', activation='softmax'))
+		# Compile model
+		model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-	# Fit model	
-	history = model.fit(X,dummy_y,validation_split=VALIDATIONPERC,nb_epoch=EPOCH,batch_size=BATCHSIZE,verbose=0)
-	print("val_acc = %.4f"%history.history["val_acc"][EPOCH-1]," acc = %.4f"%history.history["acc"][EPOCH-1])	
-	print("val_loss = %.4f"%history.history["val_loss"][EPOCH-1]," loss = %.4f"%history.history["loss"][EPOCH-1])	
-	# Show time elapsed
-	print("Elapsed time = %.2f s"%(time.time()-start_time))
-	# Storing history
-	acc = numpy.array(history.history['acc'])
-	val_acc = numpy.array(history.history['val_acc'])
-	loss = numpy.array(history.history['loss'])
-	val_loss = numpy.array(history.history['val_loss'])
-	# Plotting
-	if SHOW_ACC == 1:
-		plotGraph(regStr,nodes=hidden_nodes, vecTrain=acc, vecTest=val_acc, nameVec="acc")
-	if SHOW_LOSS == 1:
-		plotGraph(regStr,nodes=hidden_nodes, vecTrain=loss, vecTest=val_loss, nameVec="loss")
+		# Fit model	
+		history = model.fit(X,dummy_y,validation_split=VALIDATIONPERC,nb_epoch=EPOCH,batch_size=BATCHSIZE,verbose=0)
+		print("val_acc = %.4f"%history.history["val_acc"][EPOCH-1]," acc = %.4f"%history.history["acc"][EPOCH-1])	
+		print("val_loss = %.4f"%history.history["val_loss"][EPOCH-1]," loss = %.4f"%history.history["loss"][EPOCH-1])	
+		# Show time elapsed
+		print("Elapsed time = %.2f s"%(time.time()-start_time))
+		# Storing history
+		acc = numpy.array(history.history['acc'])
+		val_acc = numpy.array(history.history['val_acc'])
+		loss = numpy.array(history.history['loss'])
+		val_loss = numpy.array(history.history['val_loss'])
+		# Plotting
+		if SHOW_ACC == 1:
+			plotGraph(regStr,nodes=hidden_nodes[h], vecTrain=acc, vecTest=val_acc, nameVec="acc")
+		if SHOW_LOSS == 1:
+			plotGraph(regStr,nodes=hidden_nodes[h], vecTrain=loss, vecTest=val_loss, nameVec="loss")
 
 print("----------------------------------")
 # eof
