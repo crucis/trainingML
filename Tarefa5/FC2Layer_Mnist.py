cbc054753a223e387efd534439e1b6bb349c8cef
crucis <bandeira85@gmail.com>
2016-09-21 12:45:51 -0300

    blabla

--- Tarefa5/FC2Layer_Mnist.py
+++ Tarefa5/FC2Layer_Mnist.py
@@ -25,6 +25,7 @@ SHOW_GRAPHS = 0
 SAVE_GRAPHS = 1
 SHOW_LOSS = 1
 SHOW_ACC = 1
+SAVE_CSV = 1
 
 # fix random seed for reproducibility
 SEED = 7
@@ -36,10 +37,10 @@ EPOCH = 50
 
 
 # Model Options
-l1Reg = [0.001] # 
-l2Reg = [0.001] # 
-dropout = [0.1]
-hidden_nodes = [200, 500, 784, 1000, 2000] 
+l1Reg = [0.01]#[0,0.0001,0.0005,0.001,0.005] # 
+l2Reg = [0.01]#[0,0.0001,0.0005,0.001,0.005] # 
+dropout = [0.1]#[0.2]
+hidden_nodes = [500] #[200, 500, 784] 
 
 ########################
 #FUNCTIONS
@@ -92,8 +93,8 @@ num_pixels = X.shape[1]*X.shape[2]
 X = X.reshape(X.shape[0],num_pixels).astype('float32')
 X_test = X_test.reshape(X_test.shape[0],num_pixels).astype('float32')
 # Data Preprocessing
-X -= numpy.mean(X)
-X_test -= numpy.mean(X_test)
+#X -= numpy.mean(X)
+#X_test -= numpy.mean(X)
 # hot encode outputs
 Y = np_utils.to_categorical(Y)
 Y_test = np_utils.to_categorical(Y_test)
@@ -102,7 +103,10 @@ num_classes = Y_test.shape[1]
 print("----------------------------------")
 print("Using Batch =",BATCHSIZE ,"Epoch = ",EPOCH)
 
-regStr = " "
+#define csv variable
+if SAVE_CSV == 1:
+	csvArray = numpy.array(["hidden_nodes","l2Reg","dropout","val_acc","acc","val_loss","loss"])
+
 
 # define baseline model
 def baseline_model(nodes):
@@ -119,18 +123,16 @@ for h in range(0, len(hidden_nodes)):
 			# create model
 			model = Sequential()
 			# creates layer with hidden_nodes nodes and chooses which regularization it will use
-			model.add(Dense(hidden_nodes[h], input_dim=num_pixels, init='normal', activation='relu', W_regularizer = l1(l1Reg[i])))
-			regStr = "L1_Regularization="+str(l1Reg[i])#+" L2_Regularization="+str(l2Reg)
+			model.add(Dense(hidden_nodes[h], input_dim=num_pixels, init='he_normal', activation='relu'))#, W_regularizer = l2(l2Reg[i])))
+			regStr = "L2_Regularization="+str(l2Reg[i])#+" L2_Regularization="+str(l2Reg)
 			model.add(Dropout(dropout[j]))
 			regStr = regStr+"_Dropout=%.1f"%dropout[j]
 			# output layer
-			model.add(Dense(num_classes, init='normal', activation='softmax'))
+			model.add(Dense(num_classes, init='he_normal', activation='softmax'))
 			print("Added",regStr)
 			# Compile model
 			model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 			history = model.fit(X,Y,validation_data=(X_test,Y_test),nb_epoch=EPOCH,batch_size=BATCHSIZE,verbose=2)
-			#print("val_acc = %.4f"%history.history["val_acc"][EPOCH-1]," acc = %.4f"%history.history["acc"][EPOCH-1])	
-			#print("val_loss = %.4f"%history.history["val_loss"][EPOCH-1]," loss = %.4f"%history.history["loss"][EPOCH-1])	
 			# Show time elapsed
 			print("Elapsed time = ",str(timedelta(seconds=(time.time()-start_time))))
 			# Storing history
@@ -138,12 +140,19 @@ for h in range(0, len(hidden_nodes)):
 			val_acc = numpy.array(history.history['val_acc'])
 			loss = numpy.array(history.history['loss'])
 			val_loss = numpy.array(history.history['val_loss'])
+			# Storing in csvArray
+			if SAVE_CSV:
+				a = numpy.array([hidden_nodes[h],l1Reg[i],dropout[j],val_acc[EPOCH-1],acc[EPOCH-1],val_loss[EPOCH-1],loss[EPOCH-1]])
+				csvArray = numpy.vstack([csvArray,a])
 			# Plotting
 			if SHOW_ACC == 1:
 				plotGraph(regStr,nodes=hidden_nodes[h], vecTrain=acc, vecTest=val_acc, nameVec="acc")
 			if SHOW_LOSS == 1:
 				plotGraph(regStr,nodes=hidden_nodes[h], vecTrain=loss, vecTest=val_loss, nameVec="loss")
 
+if SAVE_CSV:
+	print("Saving results to test.csv")
+	numpy.savetxt("test.csv",csvArray,delimiter=",",fmt="%s")
 
 print("----------------------------------")
 # eof
