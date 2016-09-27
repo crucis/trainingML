464c54a10c77f682251f42b261da40357281376d
crucis <bandeira85@gmail.com>
2016-09-27 01:03:18 -0300

    Added first colorized sample

--- ColorizingNetwork/ColorizingNet_Cifar10.py
+++ ColorizingNetwork/ColorizingNet_Cifar10.py
@@ -161,15 +161,14 @@ print('acc =',acc[EPOCH-1],' val_acc =',val_acc[EPOCH-1],' loss =',loss[EPOCH-1]
 # Getting a result sample for model
 pred = model.predict(G)
 plt.imshow(G[120,0,:,:],cmap='gray')
-mkdir_p('results/Test1')
-plt.savefig('results/Test1/input.png')
+mkdir_p('results/Test2')
+plt.savefig('results/Test2/input.png')
 plt.show()
 l = pred[120].transpose(1,2,0)
 plt.imshow(l)
-mkdir_p('results/Test1')
 titlestr = 'Epochs='+str(EPOCH)
 plt.title(titlestr)
-plt.savefig('results/Test1/output.png')
+plt.savefig('results/Test2/output.png')
 plt.show()
 
 if SAVE_CSV:
