9a6a5447780efad179c3b232bc229cb3aa5ef3fd
crucis <bandeira85@gmail.com>
2016-09-26 23:43:04 -0300

    Added the folder for ColorizingNetwork

---
+++ ColorizingNetwork/ColorizingNet_Cifar10.py
@@ -0,0 +1,110 @@
+# Primeira versão da rede neural capaz de colorir fotos preto e brancas.
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.utils import np_utils
+from keras.datasets import cifar10
+from keras.regularizers import l2
+from keras.regularizers import l1
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
+import matplotlib.pyplot as plt
+import numpy
+import time
+from datetime import timedelta
+
+
+########################
+#CONSTANTS and VARIABLES
+########################
+
+# Graphs options
+SHOW_GRAPHS = 0
+SAVE_GRAPHS = 1
+SHOW_LOSS = 1
+SHOW_ACC = 1
+SAVE_CSV = 0
+
+# fix random seed for reproducibility
+SEED = 7
+numpy.random.seed(SEED)
+
+# Training Options
+BATCHSIZE = 200
+EPOCH = 10
+
+
+# Model Options
+kernel1 = [16,3] # 32 Kernels = 2x2
+kernel2 = [32,3]
+kernel3 = [64,3]
+dropout = [0.05, 0.1]
+pooling = 2
+l1Reg = 0.001
+hidden_nodes = [288] #[200, 500, 784] 
+
+########################
+#FUNCTIONS
+########################
+
+def mkdir_p(mypath):
+    #Creates a directory. equivalent to using mkdir -p on the command line'''
+
+    from errno import EEXIST
+    from os import makedirs,path
+
+    try:
+        makedirs(mypath)
+    except OSError as exc: # Python >2.5
+        if exc.errno == EEXIST and path.isdir(mypath):
+            pass
+        else: raise
+
+def rgb2gray(rgb):
+    return numpy.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+# defining function to display and/or save graphs
+def plotGraph (filename, nodes, vecTrain, vecTest, nameVec):
+	# Plot	
+	print("--Now plotting ",nameVec,"--")
+	#Plot Loss
+	plt.plot(vecTrain)
+	plt.plot(vecTest)
+	titlestr = nameVec+" for "+str(nodes)+" hidden nodes with "+filename
+	plt.title(titlestr)
+	plt.ylabel(nameVec)
+	plt.xlabel('epoch')
+	plt.legend(['train', 'test'], loc='best')
+	strAnnotation = "val_"+nameVec+"="+str(vecTest[len(vecTest)-1])
+	plt.text(2,min(vecTrain),strAnnotation, fontsize=14)
+	if SAVE_GRAPHS == 1:
+		output_dir="results/"+str(nodes)+"nodes/"+str(EPOCH)+"Epoch"
+		mkdir_p(output_dir) # Verifies if directory exists, and creates it if necessary
+		figurestr = output_dir+"/"+filename+"_"+nameVec+".png"
+		plt.savefig(figurestr)
+	if SHOW_GRAPHS == 1:
+		plt.show()
+	plt.clf()
+
+########################
+#PROGRAM
+########################
+
+# load cifar10 dataset
+(Y,a),(Y_test, a_test) = cifar10.load_data()
+X = numpy.zeros((Y.shape[0],1,Y.shape[2],Y.shape[3]))
+print('X-',X.shape)
+print('Y-',Y.shape)
+# Convert RGB to grayscale to create our input
+for i in range(0, Y.shape[0]):
+	X[i,0,:,:] = rgb2gray(Y[i,:,:,:].transpose(1,2,0))
+for i in range(0,Y_test.shape[0]):
+	X_test[i,0,:,:] = rgb2gray(Y_test[i,:,:,:]).transpose(1,2,0))
+if SAVE_CSV:
+	print("Saving results to test.csv")
+	numpy.savetxt("test.csv",csvArray,delimiter=",",fmt="%s")
+
+print("----------------------------------")
+# eof
--- Tarefa6/ConvNet_Mnist.py
+++ Tarefa6/ConvNet_Mnist.py
@@ -37,11 +37,12 @@ EPOCH = 10
 
 
 # Model Options
-kernel1 =[8,2] # 32 Kernels = 2x2
-kernel2 = [16,2]
-kernel3 = [32,2]
-dropout = [0.2]
+kernel1 = [16,3] # 32 Kernels = 2x2
+kernel2 = [32,3]
+kernel3 = [64,3]
+dropout = [0.05, 0.1]
 pooling = 2
+l1Reg = 0.001
 hidden_nodes = [288] #[200, 500, 784] 
 
 ########################
@@ -122,7 +123,7 @@ for h in range(0, len(hidden_nodes)):
 		# Convolution Layer
 		model.add(Convolution2D(kernel1[0], kernel1[1], kernel1[1], border_mode='valid', input_shape=(1, 28, 28), activation='relu'))
 		#MaxPooling
-		regStr = "MaxPool_KernelsOf"+str(kernel1[0])+"x"+str(kernel1[1])+"x"+str(kernel1[1])
+		regStr = "L1Reg_KernelsOf"+str(kernel1[0])+"x"+str(kernel1[1])+"x"+str(kernel1[1])
 		model.add(MaxPooling2D(pool_size=(pooling,pooling)))
 		# Convolution Layer
 		model.add(Convolution2D(kernel2[0], kernel2[1], kernel2[1], border_mode='valid', activation='relu'))
@@ -134,7 +135,7 @@ for h in range(0, len(hidden_nodes)):
 		regStr = regStr+"-"+str(kernel3[0])+"x"+str(kernel3[1])+"x"+str(kernel3[1])
 		# Dropout
 		model.add(Dropout(dropout[j]))
-		regStr = regStr+"_Dropout=%.2f_2HiddenLayers"%dropout[j]
+		regStr = regStr+"_Dropout=%.2f"%dropout[j]
 		# Flatten
 		model.add(Flatten()) # converts 2D matrix data to vector
 		# Fully Connected
