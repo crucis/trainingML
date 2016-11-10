a6d0a824bf791ac6ed4538d615bc02f587121e9c
crucis <bandeira85@gmail.com>
2016-11-10 18:00:57 -0200

    Ended Test 14, which is a full CIFAR10 training. Changed also the algorithm to show histogram of images

--- DCGAN-Colorizing/DCGAN_ColorizingNet_Cifar10.py
+++ DCGAN-Colorizing/DCGAN_ColorizingNet_Cifar10.py
@@ -16,7 +16,7 @@ import math
 import sys
 import time
 from datetime import timedelta
-import subprocess
+#import subprocess
 
 
 
@@ -38,13 +38,16 @@ numpy.random.seed(SEED)
 
 # Training Options
 BATCH_SIZE = 256
-EPOCH = 50
-nImages = pow(2,15)
+EPOCH = 1
+nImages = pow(2,10)
 
 # Model Options
-folder = "Test13"
+folder = "Test15"
 outDir = 'results/'+folder
 
+#### CIFAR10 classifications
+cifar10_Classes = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck'];
+chosen_Class = 'ship'
 
 ########################
 #FUNCTIONS
@@ -134,6 +137,30 @@ def save3images(inp,out,original,folder):
 
 		plt.savefig(outDir+'/samples/epoch'+str(folder)+'/sample_%s.png'%i)
 		plt.clf()
+def plotHistogram(originalImage,fakeImage, nameClass = chosen_Class,directory = outDir,folder=folder):
+	# Faz 3 histogramas, um para azul outro verde e outro vermelho
+	name = folder+'using'+nameClass
+	originalImage *= 255
+	fakeImage *= 255
+	originalImage = originalImage.astype('uint8')
+	fakeImage = fakeImage.astype('uint8')
+	numBins = 256
+
+	fO, (ax1,ax2,ax3) = plt.subplots(3, sharex=True,sharey=True)
+	ax1.hist(originalImage[:,0,:,:],numBins,color='red',alpha=0.8)
+	ax1.set_title('Original images Histogram '+name)
+	ax2.hist(originalImage[:,1,:,:],numBins,color='green',alpha=0.8)
+	ax3.hist(originalImage[:,2,:,:],numBins,color='blue',alpha=0.8)
+	plt.savefig(directory+'/OriginalHist.png')
+	plt.clf()
+
+	f1, (ax1,ax2,ax3) = plt.subplots(3, sharex=True,sharey=True)
+	ax1.hist(originalImage[:,0,:,:],numBins,color='red',alpha=0.8)
+	ax1.set_title('Generated images Histogram '+name)
+	ax2.hist(originalImage[:,1,:,:],numBins,color='green',alpha=0.8)
+	ax3.hist(originalImage[:,2,:,:],numBins,color='blue',alpha=0.8)
+	plt.savefig(directory+'/GeneratedHist.png')
+	plt.clf()
 
 ########################
 #PROGRAM
@@ -145,35 +172,36 @@ sys.stdout = Logger()
 #### load cifar10 dataset
 (Y,labels),(Y_test, labels_test) = cifar10.load_data()
 
-# Choosing only boats
-Y = Y[(labels == 8)[:,0]]
-Y_test = Y_test[(labels_test == 8)[:,0]]
+
+# Choosing only one classification
+Y = Y[(labels == cifar10_Classes.index(chosen_Class))[:,0]]
+#Y_test = Y_test[(labels_test == cifar10_Classes.index(chosen_Class))[:,0]]
 
 X = numpy.zeros((Y.shape[0],1,Y.shape[2],Y.shape[3]))
-X_test = numpy.zeros((Y_test.shape[0],1,Y.shape[2],Y.shape[3]))
+#X_test = numpy.zeros((Y_test.shape[0],1,Y.shape[2],Y.shape[3]))
 
 # Convert RGB to grayscale to create our input
 converter(X,Y)
-converter(X_test,Y_test)
+#converter(X_test,Y_test)
 
 #### Data Preprocessing
 # convert inputs and outputs to float32
 X = X.astype('float32')
 Y = Y.astype('float32')
-X_test = X_test.astype('float32')
-Y_test = Y_test.astype('float32')
+#X_test = X_test.astype('float32')
+#Y_test = Y_test.astype('float32')
 # normalize inputs and outputs from 0-255 to 0.0-1.0
 X /= 255
 Y /= 255
-X_test /= 255
-Y_test /= 255
+#X_test /= 255
+#Y_test /= 255
 
 
-
+# limits the number of images to nImages
 G = X[:nImages]
 F = Y[:nImages]
-G_test = X_test[nImages:nImages+math.ceil(nImages/5)]
-F_test = Y_test[nImages:nImages+math.ceil(nImages/5)]
+#G_test = X_test[nImages:nImages+math.ceil(nImages/5)]
+#F_test = Y_test[nImages:nImages+math.ceil(nImages/5)]
 
 
 #### Models
@@ -234,22 +262,23 @@ discriminator = discriminator_model()
 discriminator.load_weights("results/Test12/discriminator_weights")
 generator = generator_model()
 # LOADING GENERATOR FROM TEST8
-generator.load_weights("results/Test8/generator_weights")
+#generator.load_weights("results/Test8/generator_weights")
 discriminator_on_generator = generator_containing_discriminator(generator,discriminator)
 # Optimizer
 adam=Adam(lr=0.0002, beta_1=0.5, beta_2=0.999, epsilon=1e-08)
 # Compile generator
 generator.compile(loss='mean_squared_error',optimizer='nadam')
 discriminator_on_generator.compile(loss='binary_crossentropy',optimizer=adam)
-#discriminator.trainable = True
-discriminator.compile(loss='binary_crossentropy',optimizer='nadam')
+discriminator.trainable = True
+discriminator.compile(loss='binary_crossentropy',optimizer='nadam', metrics=['accuracy'])
 
 # Initialize d_loss
 d_predict_real = 0
 d_loss = 1
+d_acc = 0
 
 for epoch in range(EPOCH):
-	print("Epoch is", epoch,"of",EPOCH)
+	print("Epoch is", epoch+1,"of",EPOCH)
 	print("Number of batches",int(F.shape[0]/BATCH_SIZE))
 	start_time = time.time()
 
@@ -257,7 +286,7 @@ for epoch in range(EPOCH):
 	for index in range(int(F.shape[0]/BATCH_SIZE)):
 		image_batch = F[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
 		BW_image_batch = G[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
-		#gAlone_loss = generator.train_on_batch(BW_image_batch,image_batch)
+		gAlone_loss = generator.train_on_batch(BW_image_batch,image_batch)
 		#print("Generating images...")
 		generated_images = generator.predict(BW_image_batch)
 
@@ -272,37 +301,46 @@ for epoch in range(EPOCH):
 
 		#print("Training discriminator...")
         # Does not train discriminator if it is close to overfitting
-		if d_loss < 0.1:
+		if d_acc > 0.55:
 			discriminator.trainable = False
 		else:
 			discriminator.trainable = True
-			d_loss = discriminator.train_on_batch(M,z)
+			[d_loss, d_acc] = discriminator.train_on_batch(M,z)
 
 
 
 		for j in range(1):
 			#print("Training generator...")
 			g_loss = discriminator_on_generator.train_on_batch(BW_image_batch,[1]*BW_image_batch.shape[0])
-			print("GAN loss %.4f "%g_loss, "Discriminator loss %.4f"%d_loss, "Total: %.4f"%(g_loss+d_loss),"For batch",index)
+			print("GAN loss %.4f "%g_loss, "Discriminator loss %.4f"%d_loss,"Discriminator accuracy %.4f"%d_acc,"Generator loss %.4f"%gAlone_loss, "Total: %.4f"%(g_loss+d_loss),"For batch",index)
             #print("Generator loss %.4f"%gAlone_loss,"GAN loss %.4f "%g_loss, "Discriminator loss %.4f"%d_loss, "Total: %.4f"%(g_loss+d_loss+gAlone_loss),"For batch",index)
 	# Test if discriminator is working
-	d_predict_real = discriminator.predict(image_batch)[index]
-	print("DISCRIMINATOR_Imagem REAL=",d_predict_real)
-	print("DISCRIMINATOR_Imagem FAKE=",discriminator.predict(generator.predict(BW_image_batch))[index])
+	d_predict_real = discriminator.predict(image_batch)
+	print("DISCRIMINATOR_Imagem REAL=",d_predict_real[index])
+	g_predict_fake = generator.predict(BW_image_batch)
+	print("DISCRIMINATOR_Imagem FAKE=",discriminator.predict(g_predict_fake)[index])
 	print("GAN_Imagem FAKE=",discriminator_on_generator.predict(BW_image_batch)[index])
 
 	print("Saving weights...")
 	generator.save_weights(outDir+'/generator_weights',True)
 	discriminator.save_weights(outDir+'/discriminator_weights',True)
-	print("Saving images...")
+	print("Saving sample images...")
 	save3images(BW_image_batch,generated_images,image_batch,epoch)
-
+	print("Storing to histogram values")
+	if index == 0:
+		stored_g_predict = numpy.array(g_predict_fake)
+	else:
+		stored_g_predict = numpy.vstack(g_predict_fake)
 	print("Elapsed time in epoch = ",str(timedelta(seconds=(time.time()-start_time))))
 	print("----------------------------------")
 
 
 
-# Print important parameters to logfile.log
+# Print important parameters to logfile.log and save histogram
+print('End of training')
+print('Saving histograms')
+plotHistogram(originalImage=F,fakeImage=stored_g_predict)
+print("----------------------------------")
 
 print('Total samples = ', G.shape[0], ' Batch size =', BATCH_SIZE, ' Epochs = ', EPOCH)
 print("Generator loss %.4f "%g_loss, "Discriminator loss %.4f"%d_loss, "Total: %.4f"%(g_loss+d_loss))
@@ -316,6 +354,6 @@ print("----------------------------------")
 print("---GAN---")
 print(discriminator_on_generator.summary())
 print("----------------------------------")
-subprocess.call(['speech-dispatcher'])        #start speech dispatcher
-subprocess.call(['spd-say', '"your process has finished"'])
+#subprocess.call(['speech-dispatcher'])        #start speech dispatcher
+#subprocess.call(['spd-say', '"your process has finished"'])
 # eof
