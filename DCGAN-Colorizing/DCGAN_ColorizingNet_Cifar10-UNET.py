eb7fcbdc9ffa3c7eebb57d98fdb0fc35ef46ff8a
Pedro Bandeira de Mello Martins <bandeira85@gmail.com>
2017-01-15 23:37:28 -0200

    changed deconvolutions

--- DCGAN-Colorizing/DCGAN_ColorizingNet_Cifar10-UNET.py
+++ DCGAN-Colorizing/DCGAN_ColorizingNet_Cifar10-UNET.py
@@ -295,7 +295,7 @@ def generator_model():
 	m1 = merge([deconv0,conv4],mode='concat',concat_axis=1)
 	# 2x2
 
-	deconv1 = Deconvolution2D(256,3,3,subsample=(2,2),border_mode='same',init='he_normal',output_shape=(BATCH_SIZE,512,4,4))(deconv0)
+	deconv1 = Deconvolution2D(256,3,3,subsample=(2,2),border_mode='same',init='he_normal',output_shape=(BATCH_SIZE,256,4,4))(deconv0)
 	deconv1 = BatchNormalization(mode=2,axis=1)(deconv1)
 	deconv1 = Activation('relu')(deconv1)
 	deconv1 = Dropout(0.2)(deconv1)
@@ -304,7 +304,7 @@ def generator_model():
 
 	m1 = merge([deconv1,conv3],mode='concat',concat_axis=1)
 
-	deconv2 = Deconvolution2D(128,3,3,subsample=(2,2),border_mode='same',init='he_normal',output_shape=(BATCH_SIZE,512,8,8))(m1)
+	deconv2 = Deconvolution2D(128,3,3,subsample=(2,2),border_mode='same',init='he_normal',output_shape=(BATCH_SIZE,128,8,8))(m1)
 	deconv2 = BatchNormalization(mode=2,axis=1)(deconv2)
 	deconv2 = Activation('relu')(deconv2)
 
@@ -312,7 +312,7 @@ def generator_model():
 
 	m2 = merge([deconv2,conv2],mode='concat',concat_axis=1)
 
-	deconv3 = Deconvolution2D(64,3,3,subsample=(2,2),border_mode='same',init='he_normal',output_shape=(BATCH_SIZE,512,16,16))(m2)
+	deconv3 = Deconvolution2D(64,3,3,subsample=(2,2),border_mode='same',init='he_normal',output_shape=(BATCH_SIZE,64,16,16))(m2)
 	deconv3 = BatchNormalization(mode=2,axis=1)(deconv3)
 	deconv3 = Activation('relu')(deconv3)
 
@@ -320,7 +320,7 @@ def generator_model():
 
 	m3 = merge([deconv3,conv1],mode='concat',concat_axis=1)
 
-	deconv4 = Deconvolution2D(32,3,3,subsample=(2,2),border_mode='same',init='he_normal',output_shape=(BATCH_SIZE,512,32,32))(m3)
+	deconv4 = Deconvolution2D(32,3,3,subsample=(2,2),border_mode='same',init='he_normal',output_shape=(BATCH_SIZE,32,32,32))(m3)
 	deconv4 = BatchNormalization(mode=2,axis=1)(deconv4)
 	deconv4 = Activation('relu')(deconv4)
 
